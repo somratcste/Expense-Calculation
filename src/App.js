db33c610d9e3145469584f7816cb77775172da7d
somratcste <somratcste@gmail.com>
2021-04-15 15:48:38 +0600

    task: props uses

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
-  return (
-    <div className="App">
-      Let's Get Started
-      <ExpenseItem />
-    </div>
-  );
+
+  const expenses = [
+    { id: 'e1', title: 'Car Insurance 1', amount: 294.67, date: new Date("2021-03-28") },
+    { id: 'e2', title: 'Car Insurance 2', amount: 294.67, date: new Date("2021-03-28") },
+    { id: 'e3', title: 'Car Insurance 3', amount: 294.67, date: new Date("2021-03-28") },
+    { id: 'e4', title: 'Car Insurance 4', amount: 294.67, date: new Date("2021-03-28") },
+  ];
+
+return (
+  <div className="App">
+    Let's Get Started
+    <ExpenseItem 
+      title={expenses[0].title}
+      amount={expenses[0].amount}
+      date={expenses[0].date} />
+  </div>
+);
 }
 
 export default App;
