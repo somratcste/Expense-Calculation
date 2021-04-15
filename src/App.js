93d7f641922c0c094e7b7c20c3512f41d665ffae
somratcste <somratcste@gmail.com>
2021-04-15 14:32:24 +0600

    create first expense item component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div className="App">
       Let's Get Started
+      <ExpenseItem />
     </div>
   );
 }
