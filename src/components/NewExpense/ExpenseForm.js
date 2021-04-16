a09d062832a6f146ab22eeddd16051e1a9008a31
somratcste <somratcste@gmail.com>
2021-04-16 23:49:48 +0600

    task: pass components data up

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
     const [title, setTitle] = useState('');
     const [amount, setAmount] = useState('');
     const [date, setDate] = useState('');
@@ -25,7 +25,7 @@ const ExpenseForm = () => {
             amount: amount, 
             date: new Date(date)
         };
-        console.log(expenseData);
+        props.onSaveExpenseData(expenseData);
         setTitle('');
         setAmount('');
         setDate('');
